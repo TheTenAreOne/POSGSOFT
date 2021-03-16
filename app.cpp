f636dff024962bdbc015fcb4d490ac8315e33a87
Steven <imstevenguerrero@gmail.com>
2021-03-15 22:23:30 -0500

    add agregarObservacion, borrarObservacion, agregarCalificacion, borrarCalificacion

--- app.cpp
+++ app.cpp
@@ -15,34 +15,126 @@ App::App(){}
 //Métodos App
 
 //Verifica que la persona con idPersona haga parte del acta (ya sea director o jurado)
-bool App::verificarPersona( int idPersona, int idActa ){
+Acta* App::verificarPersona( int idPersona, int idActa ){
+    
     list <Acta> :: iterator it;
 
     //itera la lista de actas para encontrar el acta
     for( it = this->actas.begin(); it != this->actas.end(); it++ ){
+
         if( it->getId() == idActa ){
             if( it->getJurado1().getId() == idPersona || it->getJurado2().getId() == idPersona || it->getDirector().getId() == idPersona || it->getCoDirector().getId() ){
-                return true;
+                return &it;
             }else{
                 cout << "Usuario[" << idPersona << "] no hace parte del acta." << endl;
-                return false;
+                return null;
             }
         }
     }
+
     cout << "Acta[" << idActa << "] no encontrada." << endl;
-    return false;
+    return null;
 }
 
-void App::agregarObservacion( int criterio ){
+void App::agregarObservacion( Acta *pActa, int criterio, string observacion ){
+
+    list <criteriosEvaluacion> :: iterator it;
+
+    if( criterio <= criteriosEvaluacion.end() || criterio => criteriosEvaluacion.begin() ){
+
+        for( it = this->criteriosEvalucion.begin(); it != criteriosEvaluacion.end(); it++ ){
+
+            if( it == criterio){
+
+                setObservacion( int criterio, string observacion );
+
+            }
+
+        }
+
+    }
+    
+    else{
+
+        cout << "El numero de criterio se encuentra por fuera del rango almacenado."
+
+    }
 
 }
-void App::borrarObservacion( int criterio ){
+
+void App::borrarObservacion( Acta *pActa, int criterio ){
+
+    list <criteriosEvaluacion> :: iterator it;
+
+    if( criterio <= criteriosEvaluacion.end() || criterio => criteriosEvaluacion.begin() ){
+
+        for( it = this->criteriosEvalucion.begin(); it != criteriosEvaluacion.end(); it++ ){
+
+            if( it == criterio){
+
+                setObservacion( int criterio, string "" );
+
+            }
+
+        }
+
+    }
+    
+    else{
+
+        cout << "El numero de criterio se encuentra por fuera del rango almacenado."
+
+    }
 
 }
-void App::agregarCalificacion( int criterio ){
+void App::agregarCalificacion( Acta *pActa, int nCalificacion ){
+
+    list <calificaciones> :: iterator it;
+
+    if( nCalificacion <= calificaciones.end() || nCalificacion => calificaciones.begin() ){
+
+        for( it = this->calificaciones.begin(); it != calificaciones.end(); it++ ){
+
+            if( it == nCalificacion){
+
+                setCalificacion( float calificacion );
+
+            }
+
+        }
+
+    }
+    
+    else{
+
+        cout << "El numero de criterio se encuentra por fuera del rango almacenado."
+
+    }
 
 }
-void App::borrarCalificacion( int criterio ){
+void App::borrarCalificacion( Acta *pActa){
+
+    list <calificaciones> :: iterator it;
+
+    if( nCalificacion <= calificaciones.end() || nCalificacion => calificaciones.begin() ){
+
+        for( it = this->calificaciones.begin(); it != calificaciones.end(); it++ ){
+
+            if( it == nCalificacion){
+
+                setCalificacion( float 0.0 );
+
+            }
+
+        }
+
+    }
+    
+    else{
+
+        cout << "El numero de criterio se encuentra por fuera del rango almacenado."
+
+    }
 
 }
 
